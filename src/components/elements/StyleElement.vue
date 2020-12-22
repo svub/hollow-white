4a16acd7c2e9c2d7d1569401b695116565bbe422
Sven <sven@nimiq.com>
2020-12-22 20:58:32 +0100

    adding new style block element

---
+++ src/components/elements/StyleElement.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  .style(:class="element.classes")
+    TextElement.text(v-if="conditionFits" :elements="element.elements")
+</template>
+
+<script lang="ts">
+  import { Component, Vue, Prop } from 'vue-property-decorator';
+  import { Style } from '../../shared/entities';
+  import { log } from '../../shared/util';
+  import { AppState } from '../../store';
+  import TextElement from './TextElement.vue';
+
+  @Component({
+    name: 'StyleElement',
+    components: {
+      TextElement: () => import('./TextElement.vue'),
+    },
+  })
+  export default class StyleElement extends Vue {
+    @Prop(Object) private element: Style;
+  }
+
+</script>
+<style scoped lang="stylus">
+</style>
