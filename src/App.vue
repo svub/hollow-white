cdfda0b13f34323f9806ba16e91285d790cc970e
Sven <sven@nimiq.com>
2020-12-26 12:46:10 +0100

    fixing MVP: minimum styling, clean-up. scroll back to top on page change

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,80 @@
 <template web lang="pug">
-  main(:class="page")
-    transition
-      Start(key="1" v-if="page === 'start'")
-      Read(key="2" v-if="page === 'read'")
-      Tester(key="3" v-if="page === 'test'")
-    HelloWorld(:msg="msg")
+main(:class="page")
+  transition
+    Start(key="1", v-if="page === 'start'")
+    Read(key="2", v-if="page === 'read'")
+    Tester(key="3", v-if="page === 'test'")
+  //- HelloWorld(:msg="msg")
 </template>
 <template native>
   <Page>
-    <ActionBar :title="navbarTitle"/>
+    <ActionBar :title="navbarTitle" />
     <GridLayout rows="auto, auto">
-      <HelloWorld :msg="msg"/>
+      <HelloWorld :msg="msg" />
     </GridLayout>
   </Page>
 </template>
 
 <script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator';
-  import HelloWorld from '~/components/HelloWorld.vue';
-  import Start from '~/views/Start.vue';
-  import Read from '~/views/Read.vue';
-  import Tester from '~/views/Tester.vue';
-  import { State, Action } from 'vuex-class';
-  import book from './book';
-  import config from './config';
-import { warn } from './shared/util';
+import { Component, Vue } from "vue-property-decorator";
+// import HelloWorld from './components/HelloWorld.vue';
+import Start from "./views/Start.vue";
+import Read from "./views/Read.vue";
+import Tester from "./views/Tester.vue";
+import { State, Action } from "vuex-class";
+import book from "./book";
+import config from "./config";
+import { warn } from "./shared/util";
+import appState from "./store";
 
-  const { VUE_APP_MODE, VUE_APP_PLATFORM } = process.env;
+const { VUE_APP_MODE, VUE_APP_PLATFORM } = process.env;
 
-  @Component({
-    name: 'home',
-    components: {
-      HelloWorld,
-      Start,
-      Read,
-      Tester,
-    },
-  })
-  export default class App extends Vue {
-    private navbarTitle = `App.vue`;
-    private msg = `Mode=${VUE_APP_MODE} and Platform=${VUE_APP_PLATFORM}`;
+@Component({
+  name: "home",
+  components: {
+    // HelloWorld,
+    Start,
+    Read,
+    Tester,
+  },
+})
+export default class App extends Vue {
+  private navbarTitle = `App.vue`;
+  // private msg = `Mode=${VUE_APP_MODE} and Platform=${VUE_APP_PLATFORM}`;
 
-    @State page
-    @Action init
-    @Action("page") setPage;
+  @State page;
+  @Action init;
+  @Action("page") setPage;
 
-    created() {
-      this.init({ book, config });
-      if (localStorage.getItem('runTester')) {
-        warn('running tester...');
-        this.setPage('test');
-      }
+  created() {
+    this.init({ book, config });
+    console.log(this.init);
+    console.log(this.$store);
+    const testPage = localStorage.getItem("testPage");
+    if (testPage) {
+      warn("TEST: going to page ", testPage);
+      this.setPage(testPage);
     }
+
+    // for debugging and testing
+    window["appState"] = appState;
   }
+}
 </script>
 
 <style web lang="stylus">
-  @import '~styles/style-one';
+@import '~styles/reset';
+@import '~styles/typography';
+@import '~styles/layout';
+@import '~styles/style-bright';
+@import '~styles/style-dark';
 
-  .w-page
-    height 100%
-    width 100%
-
+main {
+  height 100%
+  width 100%
+  max-width 60em
+  margin auto
+}
 </style>
 <style native lang="stylus">
-  @import '~styles/style-one';
 </style>
--- src/views/Read.vue
+++ src/views/Read.vue
@@ -1,18 +1,18 @@
 <template lang="pug">
   .page.start
     .title
-      .icon icon
+      //- .icon icon
       h2(v-if="chapter.sections.indexOf(section) === 0") {{ chapter.title }}
       h3 {{ section.title }}
     TextElement.text(:elements="section.elements")
-    .buttons
-      button(v-for="link in section.next" @click="goto(link)") {{ link.title }}
+    .next.links
+      button(v-for="link in section.next" @click="next(link)") {{ link.title }}
 </template>
 
 <script lang="ts">
   import { Component, Vue, Prop } from 'vue-property-decorator';
   import { State, Action } from 'vuex-class'
-  import { Chapter, Section, Element } from '../shared/entities';
+  import { Chapter, Section, Element, Link } from '../shared/entities';
   import { log } from '../shared/util';
   import TextElement from '../components/elements/TextElement.vue';
 
@@ -27,6 +27,11 @@
     @State(state => state.section) section: Section;
     @State sectionId;
     @Action goto;
+
+    next(link: Link) {
+      this.goto(link);
+      window.scrollTo(0, 0);
+    }
   }
 </script>
 
