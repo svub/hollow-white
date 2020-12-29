55f806541f717011120130b05ea06ee9a77d02b3
Sven <sven@nimiq.com>
2020-12-29 19:20:45 +0100

    using showdown for MD>HTML conversion

--- src/components/elements/ParagraphElement.vue
+++ src/components/elements/ParagraphElement.vue
@@ -1,15 +1,20 @@
 <template lang="pug">
-p.paragraph {{ element.text }}
+p.paragraph(v-html="text")
 </template>
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
 import { Paragraph } from "../../shared/entities";
+import showdown from 'showdown';
 
 @Component({
   name: "ParagraphElement",
 })
 export default class ParagraphElement extends Vue {
   @Prop(Object) private element: Paragraph;
+
+  get text(): string {
+    return new showdown.Converter().makeHtml(this.element.text)
+  }
 }
 
 // Vue.component('paragraph-element', ParagraphElement);
