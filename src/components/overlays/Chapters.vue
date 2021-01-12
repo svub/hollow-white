3b1bf1c6ca387b543f4b13954532b2769613ad21
Sven <sven@nimiq.com>
2021-01-12 11:20:41 +0100

    basic chapters overlay

---
+++ src/components/overlays/Chapters.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+.chapters
+  .list(ref='scroll')
+    .chapter(v-for="chapter in chapters", @click="toggle(chapter.id)")
+      h3 {{ chapter.title }}
+      .sections(:class="{ open:  opened === chapter.id}")
+        .section(v-for="section in chapter.sections" @click="goto({ chapterId: chapter.id, sectionId: section.id }); overlay()")
+          h4 {{ section.title }}
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+import { Action, Getter } from "vuex-class";
+import { Chapter } from "../../shared/entities";
+import { warn } from "../../shared/util";
+
+@Component({
+  name: "Chapters",
+  components: {},
+})
+export default class Chapters extends Vue {
+  @Prop(Array) chapters: Chapter[];
+  @Action goto: Function;
+  @Action overlay: Function;
+  opened = '';
+
+  mounted() {
+    this.opened = this.chapters[this.chapters.length - 1].id;
+    const scroll = this.$refs.scroll as HTMLElement;
+    setTimeout(() => scroll.scrollTo(0, scroll.scrollHeight) , 1);
+  }
+
+  toggle(id: string) {
+    this.opened = this.opened === id ? '' : id;
+    return this.opened === id;
+  }
+}
+</script>
