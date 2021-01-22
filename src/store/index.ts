97090e01e1449ba4186daef37de53b28c217a00b
Sven <sven@nimiq.com>
2021-01-22 13:05:06 +0100

    added vuex-persist + big refactoring

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,47 +1,73 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { Book, Chapter, Link, Reference, Section, Item, State, Config, ChangeState, AddItem, RemoveItem, Option } from '../shared/entities';
-import { equal, log, warn, load, store, loadAll, logRaw } from '../shared/util';
+import { Chapter, Link, Reference, Section, Item, State, Config, ChangeState, AddItem, RemoveItem, Option } from '../shared/entities';
+// import { equal, log, warn, load, store, loadAll, logRaw } from '../shared/util';
+import { equal, log, warn, loadAll, logRaw } from '../shared/util';
 import { error } from '../shared/util';
 import { clone } from '../../../importer/src/shared/util';
 import uniq from 'lodash/uniq';
 import { optionalCallExpression } from '@babel/types';
+import VuexPersistence from 'vuex-persist';
+import book from '@/book';
+import config from '@/config';
+import { Choice } from '../../../importer/src/shared/entities';
 
 Vue.use(Vuex)
 
-export interface AppState {
+export interface Settings {
   page: string;
   overlay?: string;
-  started: boolean;
-  config: Config | null;
-  book: Book | null;
-  chapter: Chapter | null;
-  section: Section | null;
-  theme: string;
-  path: Array<Reference>;
-  items: { [id: string]: Item };
-  states: { [id: string]: State };
   options: { [id: string]: Option };
 }
 
-const state: AppState = {
-  page: 'start',
-  overlay: undefined,
-  started: false,
-  config: null,
-  book: null,
-  chapter: null,
-  section: null,
-  theme: '',
-  path: [],
-  items: {},
-  states: {},
-  options: {},
+export interface BookState {
+  position: Reference | null;
+  path: Array<Reference>;
+  items: { [id: string]: Item };
+  states: { [id: string]: State };
 }
 
-const find = (book, chapterId, sectionId): { chapter: Chapter; section: Section } => {
-  const chapter = book.chapters.find(chapter => chapter.id === chapterId) || book.chapters[0];
-  const section = chapter?.sections.find(section => section.id === sectionId) || chapter.sections[0];
+export interface AppState extends Settings, BookState {}
+
+function initialSettings(): Settings {
+  return {
+    page: 'start',
+    overlay: undefined,
+    options: {},
+  };
+}
+function initialBookState(): BookState {
+  return {
+    position: null,
+    path: [],
+    items: {},
+    states: {},
+  };
+}
+function initialState(): AppState {
+  return {
+    ...initialSettings(),
+    ...initialBookState(),
+  }
+}
+
+const state: AppState = initialState();
+
+// export interface Getters {
+//   progress({ path }, getters): Chapter[];
+//   chapter({ chapterId }, getters): Chapter;
+//   section({ sectionId }, getters): Section;
+// }
+
+// const getters: Getters = {
+
+// }
+
+export interface Position { chapter: Chapter; section: Section }
+
+const find = (chapterId?: string | null, sectionId?: string | null): Position => {
+  const chapter = book.chapters.find(chapter => chapter.id === chapterId) ?? book.chapters[0];
+  const section = chapter.sections.find(section => section.id === sectionId) ?? chapter.sections[0];
   return { chapter, section };
 };
 
@@ -54,50 +80,46 @@ export default new Vuex.Store({
     overlay(state, { overlay }) {
       state.overlay = overlay;
     },
-    start(state) {
-      store('started', state.started = true);
-    },
-    init(state, { book, config }: { book: Book; config: Config }) {
-      // restore state from local storage
-      for (const key in state) {
-        const value = load(key);
-        if (value) {
-          state[key] = value;
-        }
-      }
-      // load theme, use default, or none
-      state.theme = load('theme') ?? config?.themes?.[0] ?? '';
+    // init(state, { book, config }: { book: Book; config: Config }) {
+    //   // restore state from local storage
+    //   // for (const key in state) {
+    //   //   const value = load(key);
+    //   //   if (value) {
+    //   //     state[key] = value;
+    //   //   }
+    //   // }
+    //   // load theme, use default, or none
+    //   // state.theme = load('theme') ?? config?.themes?.[0] ?? '';
 
-      state.config = config;
-      state.book = book;
-    },
+    //   state.config = config;
+    //   state.book = book;
+    // },
     setSection(state, { chapterId, sectionId }: { chapterId: string; sectionId: string }) {
-      if (!state.book) {
-        throw new Error("Book not defined - forgot to call init?");
-      }
-      const { chapter, section } = find(state.book, chapterId, sectionId);
+      // if (!book) {
+      //   throw new Error("Book not defined - forgot to call init?");
+      // }
+      // const { chapter, section } = find(book, chapterId, sectionId);
       // log('setSection', chapterId, chapter.id, sectionId, section.id);
-      state.chapter = chapter ?? state.book.chapters[0];
-      state.section = section ?? state.chapter.sections[0];
-      log('setSection:', state.chapter.id, state.section.id);
+      // state.chapter = chapter ?? book.chapters[0];
+      // state.section = section ?? state.chapter.sections[0];
+      // log('setSection:', state.chapter.id, state.section.id);
 
       // persist
-      store('chapterId', state.chapter.id);
-      store('sectionId', state.section.id);
+      // store('chapterId', state.chapter.id);
+      // store('sectionId', state.section.id);
+
+      state.position = { chapterId, sectionId };
     },
-    addToPath(state) {
-      if (!state.chapter) {
-        throw new Error("Can not add to path, no current chapter");
+    addToPath({ position, path }) {
+      if (!position) {
+        throw new Error("Can not add to path, no current chapter/section");
       }
-      if (!state.section) {
-        throw new Error("Can not add to path, no current section");
-      }
-      const ref: Reference = { chapterId: state.chapter.id, sectionId: state.section.id };
-      if (state.path.length > 0 && equal(ref, state.path[state.path.length - 1])) {
-        warn("Trying to add same state to path", state.path, state.chapter.id, state.section.id);
+      // const ref: Reference = { chapterId: state.chapterId, sectionId: state.sectionId };
+      if (path.length > 0 && equal(position, path[path.length - 1])) {
+        warn("Trying to add same state to path", path, position);
       } else {
-        state.path.push(ref);
-        store('path', state.path);
+        path.push(position);
+        // store('path', state.path);
       }
     },
     changeState(appState, { state }: { state: ChangeState }) {
@@ -113,9 +135,9 @@ export default new Vuex.Store({
       if (state.items[item.id]) {
         return warn(`addItem: item ${ item.id } already in inventory`);
       }
-      const itemDefinition: Item | undefined = state.config!.items.find(i => i.id === item.id);
+      const itemDefinition: Item | undefined = config.items.find(i => i.id === item.id);
       if (!itemDefinition) {
-        return error(`Definition for item ${ item.id } not found!`, state.config!.items);
+        return error(`Definition for item ${ item.id } not found!`, config.items);
       }
       Vue.set(state.items, itemDefinition.id, itemDefinition);
     },
@@ -126,26 +148,33 @@ export default new Vuex.Store({
       // TODO is it enough to just remove the item? or do I need to recreate the map?
       Vue.delete(state.items, item.id);
     },
-    changeTheme(state, { theme }: { theme: string }) {
-      if (state.config?.themes && state.config?.themes?.indexOf(theme) < 0) {
-        return warn(`changeTheme: theme ${ theme } not configured`);
-      }
-      state.theme = theme;
-      store('theme', theme);
-    },
-    setOption(state, { option, choice }: { option: Option, choice: Option }) {
-      if (option.choices.indexOf(choice) < 0) {
+    // changeTheme(state, { theme }: { theme: string }) {
+    //   if (config.themes && config.themes?.indexOf(theme) < 0) {
+    //     return warn(`changeTheme: theme ${ theme } not configured`);
+    //   }
+    //   state.theme = theme;
+    //   // store('theme', theme);
+    // },
+    setOption(state, { option, choice }: { option: Option; choice: Choice }) {
+      if (!option.choices.find(c => c.id === choice.id)) {
         error('setOption: given choice is not a choice of given option.', option, choice);
       }
       Vue.set(state.options, option.id, choice.id);
-    }
+    },
+    reset(state) {
+      warn('resetting...');
+      const initial = initialBookState();
+      for (const field of Object.keys(initial)) {
+        state[field] = initial[field];
+      }
+    },
   },
   actions: {
-    init({ commit }, setup: { book: Book; config: Config }) {
-      commit('init', setup);
-      commit('setSection', loadAll('chapterId', 'sectionId'));
-      log('post init state', state.page, state.path, state.started, state.chapter, state.section);
-    },
+    // init({ commit }, setup: { book: Book; config: Config }) {
+    //   commit('init', setup);
+    //   // commit('setSection', loadAll('chapterId', 'sectionId'));
+    //   log('post init state', state.page, state.path, state.started, state.chapter, state.section);
+    // },
     page({ commit }, page) {
       if (['start', 'read', 'tester'].indexOf(page) < 0) error('Page not found', page);
       commit('page', { page });
@@ -155,7 +184,11 @@ export default new Vuex.Store({
       commit('overlay', { overlay });
     },
     start({ commit, dispatch }) {
-      commit('start');
+      const p = find();
+      commit('setSection', {
+        chapterId: p.chapter.id,
+        sectionId: p.section.id,
+      });
       dispatch('page', 'read');
     },
     goto({ commit }, link: Link) {
@@ -172,29 +205,46 @@ export default new Vuex.Store({
     removeItem({ commit }, payload: { item: RemoveItem }) {
       commit('removeItem', payload);
     },
-    changeTheme({ commit }, theme: string) {
-      commit('changeTheme', { theme });
-    },
-    setOption({ commit, state }, payload: { option: Option, choice: Option }) {
+    // changeTheme({ commit }, theme: string) {
+    //   commit('changeTheme', { theme });
+    // },
+    setOption({ commit }, payload: { option: Option; choice: Choice }) {
+    // setOption({ commit }, payload: { option: Option; choice: Choice }) {
       commit('setOption', payload);
-    }
+    },
+    reset({ commit, dispatch }) {
+      commit('reset');
+      dispatch('page', 'start');
+      dispatch('overlay', '');
+    },
   },
   getters: {
-    progress({ path, book }): Chapter[] {
-      if (book === undefined) error('book not loaded');
-      return logRaw('progress',
-        uniq(path.map(ref => ref.chapterId))
-        .map(id => book!.chapters.find(chapter => chapter.id === id))
-        .filter(chapter => !!chapter)
-        .map(chapter => clone(chapter)!)
-        .map((chapter: Chapter) => {
-          chapter.sections = chapter.sections
-            .filter(section => !!path.find(r => r.chapterId === chapter.id && r.sectionId === section.id));
-            // .map(section => { return { id: section.id, title: section.title, elements: [], next: [] } });
-          return chapter;
-        }));
+    progress({ path }, getters): Position[] {
+      // return logRaw('progress',
+      //   uniq(path.map(ref => ref.chapterId))
+      //   .map(id => book!.chapters.find(chapter => chapter.id === id))
+      //   .filter(chapter => !!chapter)
+      //   .map(chapter => clone(chapter)!)
+      //   .map((chapter: Chapter) => {
+      //     chapter.sections = chapter.sections
+      //       .filter(section => !!path.find(r => r.chapterId === chapter.id && r.sectionId === section.id));
+      //       // .map(section => { return { id: section.id, title: section.title, elements: [], next: [] } });
+      //     return chapter;
+      //   }));
+      return (path.map(ref => find(ref.chapterId, ref.sectionId)));
+    },
+    position({ position }, getters): Position {
+      return find(position?.chapterId, position?.sectionId);
+    },
+    started({ position }): boolean {
+      return !!position;
     }
   },
   modules: {
-  }
+  },
+  plugins: [
+    new VuexPersistence<AppState>({
+      storage: window.localStorage,
+    }).plugin
+  ],
 })
