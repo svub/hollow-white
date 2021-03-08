7899965ea49112b3fcd5e3dd79f23723d08a1152
Sven <sven@nimiq.com>
2021-03-07 18:27:05 -0600

    bug-fixing credits

--- src/components/overlays/Credits.vue
+++ src/components/overlays/Credits.vue
@@ -11,20 +11,18 @@ import { Component, Vue } from 'vue-property-decorator';
 import { Section, Specials } from '../../shared/entities';
 import { error, logJson } from '../../shared/util';
 import book from "../../book";
+import TextElement from '../elements/TextElement.vue';
 
 @Component({
   name: 'Credits',
-  components: {}
+  components: { TextElement }
 })
 export default class Credits extends Vue {
   book = book;
 
   get credits(): Section {
     logJson('Credits', this.book.specials, this.book!.specials[Specials.credits]);
-    if (this.book!.specials[Specials.credits] !== undefined) {
-      error('Credits not found!');
-    }
-    return this.book!.specials[Specials.credits];
+    return this.book!.specials[Specials.credits] ?? error('Credits not found!');
   }
 }
 </script>
