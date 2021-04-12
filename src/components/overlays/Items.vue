7044d371c8a7e978071e058024f54b1f60888453
Sven <sven@nimiq.com>
2021-04-12 16:08:24 +0200

    new item overlay + styling

--- src/components/overlays/Items.vue
+++ src/components/overlays/Items.vue
@@ -1,20 +1,29 @@
 <template lang="pug">
 .items
-  .item(v-for="item in items")
-    .number #[span.current {{ itemIndex(item) }}] #[span.total {{ totalItems }}]
-    h3 {{ item.title }}
-    img(:src="item.media")
-    p(v-if="item.description") {{ item.description }}
-    p(v-if="item.category") {{ item.category }}
+  .item(v-for="item in items" :class="[item.id, item.category].join(' ')")
+    .title
+      .number #[span.current {{ itemIndex(item) }}] #[span.total {{ totalItems }}]
+      h3 {{ item.title }}
+
+    .content
+      TextElement.description(:elements="item.elements")
+      .media(v-if="item.mediaUrl" :class="item.mediaType")
+        a.link(href="item.mediaUrl") {{ item.title }}
+        audio(v-if="item.mediaType == 'audio'" controls :src="item.mediaUrl")
+          Your browser does not support embedded audio.
+        video(v-if="item.mediaType == 'video'" controls)
+          source(:src="item.mediaUrl")
+          Your browser does not support embedded video.
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator';
+import TextElement from '../elements/TextElement.vue';
 import { Item } from '../../shared/entities';
 
 @Component({
   name: 'Items',
-  components: {}
+  components: { TextElement }
 })
 export default class Items extends Vue {
   @Prop(Array) items: Item[];
