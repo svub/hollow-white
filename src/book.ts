3ad39fba1278561afd08d9ecce984a54f8dda404
Sven <sven@nimiq.com>
2021-07-20 19:14:55 +0200

    book update

--- src/book.ts
+++ src/book.ts
@@ -14058,7 +14058,8 @@ const book: Book = (({
    },
    {
     "id": "lordneon",
-    "category": "",
+    "mediaType": "audio",
+    "mediaUrl": "/assets/items/lord-neon.mp3",
     "title": "Lord Neon",
     "elements": [
      {
