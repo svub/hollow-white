8ce85218b3eaa2d71f85bce98eb287c3ccceac74
svub <svub@no.email>
2022-10-13 13:01:54 +0300

    added special links section to imprint and credits (to be generalized!)

--- src/components/overlays/Credits.vue
+++ src/components/overlays/Credits.vue
@@ -1,11 +1,14 @@
 <template lang="pug">
 .credits
   TextElement.text(:elements="credits.elements")
+  .next.links(v-if="credits.next && credits.next.length > 0" :class="'count-'+credits.next.length")
+    button(v-for="link in credits.next" @click="open(link)") {{ link.title }}
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import { Section, Specials } from '../../shared/entities';
+import { Action } from 'vuex-class';
+import { Functions, Overlays, Section, SpecialLink, Specials } from '../../shared/entities';
 import { error, logJson } from '../../shared/util';
 import book from "../../book";
 import TextElement from '../elements/TextElement.vue';
@@ -15,11 +18,30 @@ import TextElement from '../elements/TextElement.vue';
   components: { TextElement }
 })
 export default class Credits extends Vue {
+  @Action overlay!: Function;
   private book = book;
 
   get credits(): Section {
     logJson('Credits', this.book.specials, this.book.specials[Specials.credits]);
     return this.book.specials[Specials.credits] ?? error('Credits not found!');
+  }
+  
+  open(link: SpecialLink) {
+    if (link.id === Functions.share) return this.share(link.title, link.data);
+    if (Overlays[link.id]) return this.overlay(link.id);
+  }
+
+  async share(title: string, url?: string) {
+    if (!url) error('Credits.share: url not defined', title);
+    const data = { title, url };
+    let nativeFailed = false;
+    if (navigator.share) {
+      try { await navigator.share(data); }
+      catch (e) { nativeFailed = true }
+    }
+    if (!navigator.share || nativeFailed) {
+      this.overlay({ overlay: Overlays.shareOverlay, data });
+    }
   }
 }
 </script>
--- src/components/overlays/Imprint.vue
+++ src/components/overlays/Imprint.vue
@@ -1,11 +1,14 @@
 <template lang="pug">
 .imprint
   TextElement.text(:elements="imprint.elements")
+  .next.links(v-if="imprint.next && imprint.next.length > 0" :class="'count-'+imprint.next.length")
+    button(v-for="link in imprint.next" @click="open(link)") {{ link.title }}
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import { Section, Specials } from '../../shared/entities';
+import { Action } from 'vuex-class';
+import { Functions, Overlays, Section, SpecialLink, Specials } from '../../shared/entities';
 import { error, logJson } from '../../shared/util';
 import book from "../../book";
 import TextElement from '../elements/TextElement.vue';
@@ -15,11 +18,30 @@ import TextElement from '../elements/TextElement.vue';
   components: { TextElement }
 })
 export default class Imprint extends Vue {
+  @Action overlay!: Function;
   private book = book;
 
   get imprint(): Section {
     logJson('Imprint', this.book.specials, this.book.specials[Specials.imprint]);
     return this.book.specials[Specials.imprint] ?? error('Imprint not found!');
+  }
+
+  open(link: SpecialLink) {
+    if (link.id === Functions.share) return this.share(link.title, link.data);
+    if (Overlays[link.id]) return this.overlay(link.id);
+  }
+
+  async share(title: string, url?: string) {
+    if (!url) error('Imprint.share: url not defined', title);
+    const data = { title, url };
+    let nativeFailed = false;
+    if (navigator.share) {
+      try { await navigator.share(data); }
+      catch (e) { nativeFailed = true }
+    }
+    if (!navigator.share || nativeFailed) {
+      this.overlay({ overlay: Overlays.shareOverlay, data });
+    }
   }
 }
 </script>
