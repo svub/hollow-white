da4f298b91f49fe56ff661e82c97ed408eaad150
svub <svub@no.email>
2022-11-11 16:57:35 +0100

    typescript update

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
-    '@babel/plugin-syntax-dynamic-import'
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import', 
   ],
   presets: [
     process.env.VUE_PLATFORM === 'web' ? '@vue/app' : {}, 
